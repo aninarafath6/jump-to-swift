99a7170e1ff4d360810348ea9f4a84e4f8a68ab8
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 16:06:30 +0530

    array count and appent function

--- day-3.playground/Contents.swift
+++ day-3.playground/Contents.swift
@@ -5,3 +5,14 @@ import Cocoa
 var array1 =  Array<String>()
 var array2 = [String]()
 var array:Array<String> = []
+
+// examples
+var months:Array<String>  = ["jan","feb","mar"]
+
+
+// append function is used for add an element to array
+months.append("aprl")
+print("Array: \(months)")
+// count is used to get length of array
+var length:Int = months.count
+print("size : \(length)")
