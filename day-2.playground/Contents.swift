1add77de73731f042f37f123219a0e30048f6dc6
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 16:00:22 +0530

    boolean functions

--- day-2.playground/Contents.swift
+++ day-2.playground/Contents.swift
@@ -7,3 +7,14 @@ print("is String is empty: \(fileName.isEmpty)")
 print("to LoweverCase : \(fileName.lowercased())")
 print("to upperCase: \(fileName.uppercased())")
 print("length of the string : \(fileName.count)")
+
+
+// boolean functions
+var number = 7
+var c:String = number.isMultiple(of: 3) ? "is":"not"
+var status:Bool = true
+// toggle boolen expression
+status.toggle()
+
+print(status)
+print("\(number) is \(c) a multiple of 3")
