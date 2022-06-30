6630931cc5b2a55f61cf555fcc1bcd7fdcfaf6bf
aninarafath6 <arafathanin6@gmail.com>
2022-07-01 00:58:13 +0530

    for loop

--- day-6.playground/Contents.swift
+++ day-6.playground/Contents.swift
@@ -1,3 +1,50 @@
 import Cocoa
 
 var greeting = "day 6"
+
+var users = ["anin","arafath","Sulthan"]
+
+for user in users{
+    print("user is \(user)")
+}
+
+var actors  = [
+    "Vijay":[
+        "age":"22",
+        "location":"Tamil nadu"
+    ],
+    "mammoty":[
+        "age":"52",
+        "location":"kerala"
+    ],
+    "mohanlal":[
+        "age":"24",
+        "location":"malappuram"
+    ],
+]
+
+
+for actor in actors{
+    print(actor.value)
+}
+
+
+for i in 1...10
+{
+    print(i)
+}
+
+for j in 1..<10 {
+    print(j)
+}
+
+
+for i in 1...10 {
+    print("table of \(i)")
+    print("================\n")
+    for j in 1...10 {
+        print("\(i) x \(j) = \(i*j)")
+    }
+    print("=================\n")
+
+}
