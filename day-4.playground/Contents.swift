1e2d7b51d573edf4f37a8953bb4d0facaa001c87
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 18:52:14 +0530

    checkpoint 2

--- day-4.playground/Contents.swift
+++ day-4.playground/Contents.swift
@@ -17,7 +17,7 @@ enum FileStatus {
 var status:FileStatus = FileStatus.Uploading
 status = .Downloading
 
-print(status)
+//print(status)
 
 
 var array1:[String] = ["anin","arafath"]
@@ -29,3 +29,12 @@ var array4 = Array<String>()
 
 var user:[String:String] = ["name":"anin arafath"]
 var dictionary:Dictionary<String,Int> = ["age":22]
+
+
+
+// check point
+var array:[Int] = [1,1,1,2,]
+var unqList = Set(array)
+
+print("unique items count: \(unqList.count)")
+print("total array count :\(array.count)")
