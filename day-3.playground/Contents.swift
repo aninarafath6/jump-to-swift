ab96ced7462e589e6590ef25308f77ae7b9c38e5
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 16:07:08 +0530

    array remove functions

--- day-3.playground/Contents.swift
+++ day-3.playground/Contents.swift
@@ -16,3 +16,15 @@ print("Array: \(months)")
 // count is used to get length of array
 var length:Int = months.count
 print("size : \(length)")
+
+
+// Array remove elements function
+var actors:Array<String>  = ["Mammoty", "Monhanlal","privithi Raj","Tovino Thomas"]
+print(actors)
+// remove a item from specific index
+actors.remove(at: 1) // remove the value of "Mohanal"
+print(actors)
+// common remove elements functions
+actors.removeLast()
+actors.removeFirst()
+actors.removeAll()
