83c79e9551866cc052438e9b4289dac2095ac852
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 15:59:46 +0530

    string funcitons

--- day-2.playground/Contents.swift
+++ day-2.playground/Contents.swift
@@ -1,3 +1,9 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var fileName:String = "Logo.jpg"
+// string functions
+print("is extenstion is png :  \(fileName.hasSuffix("png"))")
+print("is String is empty: \(fileName.isEmpty)")
+print("to LoweverCase : \(fileName.lowercased())")
+print("to upperCase: \(fileName.uppercased())")
+print("length of the string : \(fileName.count)")
