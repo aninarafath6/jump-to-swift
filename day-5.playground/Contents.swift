72978a3cfeb02c56dbe929cfdadb6ac525721fc1
aninarafath6 <arafathanin6@gmail.com>
2022-06-30 21:12:03 +0530

    if module

--- day-5.playground/Contents.swift
+++ day-5.playground/Contents.swift
@@ -1,3 +1,28 @@
 import Cocoa
 
 var greeting = "Day 5"
+
+// conditions
+var mark = 50
+
+
+if mark < 50 {
+    print("failed")
+}else{
+    print("passed")
+}
+
+
+//we can combare enumurators by using Comparable annotation
+enum Sizes:Comparable {
+    case small
+    case medium
+    case large
+}
+
+
+var big = Sizes.large
+var notBig = Sizes.small
+
+
+print(big<notBig)
